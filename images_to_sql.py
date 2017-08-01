f70de2ef19f554743d9122d9b277aa3ddcf1d73b
ivanhigueram <ivanhigueram@gmail.com>
2017-08-01 09:51:26 +0000

    Update to intersection GBDX data and :ship: positions
    get_images_metadata.py now uploads the metadata to the db instance
    and runs a intersection between AIS and GBDX with a timestamp difference
    of 120 secs (2 minutes --- really, lah?). A gbdx_process function is
    laking to take this data and retrieve cropped images of vessels in the
    whole :earth_americas:.

--- images_to_sql.py
+++ images_to_sql.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
+import src.utils
+from src.utils import db_connect
+
+engine_input = db_connect.alchemy_connect()
+conn_input = engine_input.connect()
 
 
 
