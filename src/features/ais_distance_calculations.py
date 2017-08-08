fixed ais distance calculation --needs code review + cleaning

src/features/ais_distance_calculations.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from os import remove
 
 import shapely as sp
 import cartopy.io.shapereader as shpreader
@@ -18,13 +17,10 @@ from scipy import spatial
 from sklearn.neighbors import NearestNeighbors, BallTree
 from pyproj import Proj, transform
 
-import geopy
-from geopy.distance import great_circle, vincenty
-
 from math import *
 
 coastline = np.load(os.path.join(os.path.dirname(__file__),
-                    '../data/shape_files/coast_coords_50m.npy')
+                    '../data/shape_files/coast_coords_10m.npy'))
 
 ports = np.load(os.path.join(os.path.dirname(__file__),
                 '../data/shape_files/ports_coords.npy'))
@@ -46,18 +42,19 @@ def extract_geom_meta(country):
     of tuples and the country name as the last element.
     '''
     geoms = country.geometry
+    coords = np.empty(shape=[0, 2])
     for geom in geoms:
-        x,y = geom.exterior.xy
+        coords = np.append(coords, geom.exterior.coords, axis = 0)
 
-    meta_data = country.attributes["admin"]
-    return [*zip(x,y), meta_data]
+    country_name = country.attributes["ADMIN"]
+    return [coords, country_name]
 
 def save_coastline_shape_file():
     '''
     store shp files locally, this functions will download
     shapefiles for the whole planet.
     '''
-    ne_earth = shpreader.natural_earth(resolution = '50m',
+    ne_earth = shpreader.natural_earth(resolution = '10m',
                                        category = 'cultural',
                                        name='admin_0_countries')
     reader = shpreader.Reader(ne_earth)
@@ -67,8 +64,8 @@ def save_coastline_shape_file():
     coords_countries = np.vstack([[np.array(x[:-1]), x[-1]]
                                     for x in world_geoms])
     coastline = np.save(os.path.join(os.path.dirname(__file__),
-                        '../data/shape_files/coast_coords_50m.npy'
-                        ), coords_countries)
+                        '../data/shape_files/coast_coords_10m.npy')
+                        , coords_countries)
     print('Saving coordinates (...)')
 
 def save_ports_shape_file():
@@ -97,11 +94,10 @@ def distance_to_shore(lon, lat):
     to shore. It will contain and ID for AIS points and
     the distance to the nearest coastline point.
     '''
-    coastline_coords = np.vstack([np.flip(x[0], axis=1) for x in coastline])
-    countries = np.hstack([np.repeat(str(y[1]), len(y[0])) for y in coastline])
+    coastline_coords = np.vstack([np.flip(x[0][0], axis=1) for x in coastline])
+    countries = np.hstack([np.repeat(str(x[1]), len(x[0][0])) for x in coastline])
+    tree = BallTree(np.radians(coastline_coords), metric='haversine')
     coords = pd.concat([np.radians(lat), np.radians(lon)], axis=1)
-    tree = BallTree(np.radians(coastline_coords),
-                    metric='haversine')
     dist, ind = tree.query(coords, k=1)
     df_distance_to_shore = pd.Series(dist.flatten()*6371, name='distance_to_shore')
     df_countries = pd.Series(countries[ind].flatten(), name='shore_country')
@@ -123,6 +119,7 @@ def distance_to_port(lon, lat):
 
 if __name__ == "__main__":
     #save_coastline_shape_file()
+    # test points
     print(distance_to_shore(pd.Series(178.42), pd.Series(-18.1270713806)))
     print(distance_to_port(pd.Series(178.42), pd.Series(-18.1270713806)))
     print(distance_to_shore(pd.Series(-9.89), pd.Series(38.70)))
@@ -131,3 +128,5 @@ if __name__ == "__main__":
     print(distance_to_port(pd.Series(-79.049683), pd.Series(4.328306)))
     print(distance_to_shore(pd.Series(-80.384521), pd.Series(0.058747)))
     print(distance_to_port(pd.Series(-80.384521), pd.Series(0.058747)))
+    print(distance_to_shore(pd.Series(160), pd.Series(-35)))
+    print(distance_to_port(pd.Series(160), pd.Series(-35)))