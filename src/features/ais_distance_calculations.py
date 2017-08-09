976d3af5e35e1a326fdcd845347cf264ba04d4d5
william-grimes <will-grimes@gmx.com>
2017-08-09 10:46:10 +0000

    improved docstrings

--- src/features/ais_distance_calculations.py
+++ src/features/ais_distance_calculations.py
@@ -27,19 +27,19 @@ ports = np.load(os.path.join(os.path.dirname(__file__),
 
 def proj_arr(points, proj_to):
     """
-    Project geographic co-ordinates to get cartesian x,y
-    Transform(origin|destination|lon|lat) to meters.
+    project geographic co-ordinates to get cartesian x,y
+    transform(origin|destination|lon|lat) to meters.
     """
     inproj = Proj(init='epsg:4326')
     outproj = Proj(init=proj_to)
     func = lambda x: transform(inproj, outproj, x[0], x[1])
     return np.array(list(map(func, points)))
 
-def extract_geom_meta(country):
+def extract_coords_and_country(country):
     '''
-    extract from each geometry the name of the country
-    and the geom_point data. The output will be a list
-    of tuples and the country name as the last element.
+    extract from Natural Earth shape file multipolygons an array of
+    coordinates for each country. The output will be a list of tuples
+    and the country name as the last element.
     '''
     geoms = country.geometry
     coords = np.empty(shape=[0, 2])
@@ -51,18 +51,18 @@ def extract_geom_meta(country):
 
 def save_coastline_shape_file():
     '''
-    store shp files locally, this functions will download
-    shapefiles for the whole planet.
+    function to download a shape file from Natural Earth of coastlines
+    with cultural (national) boundaries, at a 10m resolution. The 
+    shape file is then stored locally.
     '''
     ne_earth = shpreader.natural_earth(resolution = '10m',
                                        category = 'cultural',
                                        name='admin_0_countries')
     reader = shpreader.Reader(ne_earth)
     countries = reader.records()
-    # extract and create separate objects
-    world_geoms = [extract_geom_meta(country) for country in countries]
+    world_geoms = [extract_coords_and_country(country) for country in countries]
     coords_countries = np.vstack([[np.array(x[:-1]), x[-1]]
-                                    for x in world_geoms])
+                                 for x in world_geoms])
     coastline = np.save(os.path.join(os.path.dirname(__file__),
                         '../data/shape_files/coast_coords_10m.npy')
                         , coords_countries)
@@ -70,44 +70,48 @@ def save_coastline_shape_file():
 
 def save_ports_shape_file():
     '''
-    store shp files locally, this functions will download
-    shapefiles for the whole planet.
+    function to download a shape file from the World Ports Index and
+    store it locally.
     '''
     url = "https://msi.nga.mil/MSISiteContent/StaticFiles/NAV_PUBS/WPI/WPI_Shapefile.zip"
     context = ssl._create_unverified_context()
     with urllib.request.urlopen(url, context=context) \
 	as response, open('world_port_data.zip', 'wb') as out_file:
-        data = response.read() # a `bytes` object
+        data = response.read()
         out_file.write(data)
-    #Unzip the file and open it
+
+    #unzip the file and open it
     zip_ref = zipfile.ZipFile('world_port_data.zip', 'r')
     zip_ref.extractall('world_port_data')
     zip_ref.close()
     ports = DBF("world_port_data/WPI.dbf", load = True)
     coords = np.asarray([(i['LONGITUDE'], i['LATITUDE']) for i in ports])
     coords = np.save(('../data/shape_files/ports_coords.npy'), coords)
-    print('Saving coordinates (...)')
+    print('Saving ports coordinates (...)')
 
 def distance_to_shore(lon, lat):
     '''
-    This function will create a numpy array of distances
-    to shore. It will contain and ID for AIS points and
-    the distance to the nearest coastline point.
+    this function takes a pandas series of longiutde and latitudes and
+    returns the distance to the closest point on land in land, as well
+    as the country of that land mass. This uses a ball tree search
+    approach in radians accounting for the curvature of the Earth by
+    calculating the haversine metric for each pair of points
     '''
     coastline_coords = np.vstack([np.flip(x[0][0], axis=1) for x in coastline])
     countries = np.hstack([np.repeat(str(x[1]), len(x[0][0])) for x in coastline])
     tree = BallTree(np.radians(coastline_coords), metric='haversine')
     coords = pd.concat([np.radians(lat), np.radians(lon)], axis=1)
     dist, ind = tree.query(coords, k=1)
-    df_distance_to_shore = pd.Series(dist.flatten()*6371, name='distance_to_shore')
+    df_distance_to_shore = pd.Series(dist.flatten()*6371, # radius of earth
+                                     name='distance_to_shore')
     df_countries = pd.Series(countries[ind].flatten(), name='shore_country')
     return pd.concat([df_distance_to_shore, df_countries], axis=1)
 
 def distance_to_port(lon, lat):
     '''
-    This function will create a numpy array of distances to ports.
-    It will contain an ID for AIS points and
-    the distance to the nearest port point.
+    this function calculates for a pandas series of longitude and
+    latitudes the distance to the closest port in kilometeres, according
+    to the World PortIndex database.
     '''
     ports_flip = np.flip(ports, axis=1)
     coords = pd.concat([np.radians(lat), np.radians(lon)], axis=1)
