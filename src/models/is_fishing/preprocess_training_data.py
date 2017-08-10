4ce15fd83993ff81b6d22384b553618b57dc6137
william-grimes <will-grimes@gmx.com>
2017-08-10 11:20:37 +0000

    Chunk and parallellise data input and output for preprocessing created a db_manipulate module for parallelisation of PSQL database with pandas. Adjusted preprocessing to create features in parallel.

--- src/models/is_fishing/preprocess_training_data.py
+++ src/models/is_fishing/preprocess_training_data.py
@@ -1,58 +1,83 @@
 """
-Script to clean training data and create features for training is_fishing
-model. This script loads data from a PSQL database in chunks performs a
-series of operations on pandas data frames and then reuploads the data to 
-a new table in the PSQL database. These operations include calculations of 
-the elevation of the sun and whether it is day or night.
+Functions to perform data transformation on training and test data
+for modelling. Uses the db_manipulate module to run these functions
+in parallel.
 
-    1. read training data chunk
-    2. drop duplicate rows
-    3. drop nas
-    4. combine labels
-    5. create utc_timestamp
-    6. create sun_height
-    7. create day variable
-    8. upload chunk in new table
+Notes:
+    EEZ is defined as 200 nautical miles or 370 km
+    Day or night is defined by whether sun is above or below horizon
+
 """
 
-import os
 import numpy as np
 import pandas as pd
-import datetime as dt
 
-import utils
-from utils import db_connect
+from utils import db_manipulate
+from src.features.ais_distance_calculations import distance_to_port, distance_to_shore
+from src.features.ais_time_calculations import  epoch_to_utc_timestamp, sun_altitude, day_or_night, epoch_to_localtime
 
-from src import features
-from src.features.ais_distance_calculations import *
-from src.features.ais_time_calculations import  epoch_to_utc_timestamp, \
-        sun_altitude, day_or_night, epoch_to_localtime
+def preprocess_training_data(chunk):
+    """
+    Create features for modelling on training data from Kristina
+    Boerder. Includes a distance_to_shore and distance_to_port
+    calculation from ais_distance_calculations.
 
-engine_input = db_connect.alchemy_connect()
-conn_input = engine_input.connect()
-
-engine_output = db_connect.alchemy_connect()
-conn_output = engine_output.connect()
-
-query_read = "SELECT * \
-              FROM ais_training_data.alex_crowd_sourced;"
-
-start = dt.datetime.now()
-size_chunk = 10000
-j = 0
-
-for chunk in pd.read_sql_query(query_read, conn_input, chunksize=size_chunk):
-    chunk.dropna(how='any', inplace=True)
-    chunk.drop_duplicates(inplace=True)
-    chunk.replace({'is_fishing': {-1: 0}}, inplace=True)
+    Where is_fishing label is -1, i.e. conflict in labelling data has been
+    removed from training.
+    """
     chunk['utc_timestamp'] = chunk.apply(
         lambda x: epoch_to_utc_timestamp(x.timestamp), axis = 1)
     chunk['sun_height'] = chunk.apply(
         lambda x: sun_altitude(x.timestamp, x.lon, x.lat, epoch=True), axis = 1)
-    print("calculated sun_height")
     chunk['day'] = chunk.apply(lambda x: day_or_night(x.sun_height), axis = 1)
-    chunk.to_sql('alex_crowd_sourced_features', conn_output,
-                 schema='ais_training_data', if_exists= 'append', index=False)
-    j+=1
-    print('{} seconds: completed {} rows'.format(
-        (dt.datetime.now() - start).seconds, j*size_chunk))
+    df = distance_to_shore(chunk.lon, chunk.lat)
+    df.reset_index(drop=True, inplace=True)
+    chunk.reset_index(drop=True, inplace=True)
+    chunk = pd.concat([chunk, df], axis=1)
+    chunk['distance_to_port'] = distance_to_port(chunk.lon, chunk.lat)
+    chunk['in_eez'] = np.where(chunk['distance_to_shore']<=370, 1, 0)
+    chunk.dropna(how='any', inplace=True)
+    chunk.drop_duplicates(inplace=True)
+    chunk.drop(chunk[chunk['is_fishing'] == -1].index.tolist(), inplace=True)
+    return chunk
+
+def preprocess_test_data(chunk):
+    """
+    Create features for modelling on test data from Spire. Includes a
+    distance_to_shore and distance_to_port calculation from
+    ais_distance_calculations.
+
+    Where is_fishing label is -1, i.e. conflict in labelling data has been
+    removed from training.
+    """
+    chunk['sun_height'] = chunk.apply(
+        lambda x: sun_altitude(x.timestamp, x.longitude, x.latitude), axis = 1)
+    chunk['day'] = chunk.apply(
+        lambda x: day_or_night(x.sun_height), axis = 1)
+    df = distance_to_shore(chunk.longitude, chunk.latitude)
+    df.reset_index(drop=True, inplace=True)
+    chunk.reset_index(drop=True, inplace=True)
+    chunk = pd.concat([chunk, df], axis=1)
+    chunk['distance_to_port'] = distance_to_port(
+        chunk.longitude, chunk.latitude)
+    chunk['in_eez'] = np.where(chunk['distance_to_shore']<=370, 1, 0)
+    chunk = chunk[['mmsi','timestamp','longitude','latitude', 'speed', 'course',
+                   'sun_height', 'day', 'distance_to_shore', 'shore_country',
+                   'in_eez', 'distance_to_port']]
+    return chunk
+
+if __name__ == '__main__':
+    #table_read = "SELECT * FROM ais_training_data.alex_crowd_sourced;"
+    #db_manipulate.loop_chunks(table_read,
+    #                           preprocess_training_data,
+    #                           'ais_training_data',
+    #                           'alex_crowd_sourced_features_2',
+    #                           parallel=True)
+
+    #table_read = "SELECT * FROM ais_messages.full_year_position;"
+    #db_manipulate.loop_chunks(table_read,
+    #                          preprocess_test_data,
+    #                          'ais_features',
+    #                          'full_year_position',
+    #                          30000000,
+    #                          parallel=True)
