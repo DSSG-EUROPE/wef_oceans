3fe2df665749504b574bc2871502325710572106
ivanhigueram <ivanhigueram@gmail.com>
2017-08-14 11:59:55 +0000

    Fixed files, again

--- src/models/is_fishing/train_is_fishing.py
+++ src/models/is_fishing/train_is_fishing.py
@@ -18,11 +18,24 @@ import sklearn
 import itertools
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
+from sklearn.metrics import log_loss
 from sklearn.externals import joblib
 
+<<<<<<< HEAD
 import config
+=======
+import ml_config
+
+#features = ml_config.params['features']
+features = ['distance_from_shore', 'distance_from_port' ,'speed', 'course']
+
+
+df = db_connect.query('SELECT * FROM ais_training_data.alex_crowd_sourced \
+                        where is_fishing != -1 order by timestamp asc;')
+>>>>>>> technical_documentation
 
 def get_training_data():
     features = list(itertools.chain.from_iterable(config.features.values()))
@@ -34,6 +47,7 @@ def get_training_data():
     df = pd.concat([df[features], df['is_fishing']], axis=1)
     return df
 
+<<<<<<< HEAD
 
 df = get_training_data(all_features)
 
@@ -53,3 +67,23 @@ precision_score(y_test, predictions)
 #0.99375462240118329 precision model_1
 
 joblib.dump(model, '../../../models/model_1.pkl')
+=======
+tscv = TimeSeriesSplit(n_splits=5)
+
+X, y = df[features], df['is_fishing']
+
+
+for train_index, test_index in tscv.split(X):
+    
+    model = RandomForestClassifier(n_estimators=2000, n_jobs=-1)
+    
+    print(train_index, test_index)
+    X_train, X_test = X.ix[train_index], X.ix[test_index]
+    y_train, y_test = y.ix[train_index], y.ix[test_index]
+    
+    model.fit(X_train, y_train)
+    predictions = model.predict_proba(X_test)
+    predictions_labels = model.predict(X_test)
+    print(precision_score(y_test, predictions_labels))
+    print(log_loss(y_test, predictions))
+>>>>>>> technical_documentation
