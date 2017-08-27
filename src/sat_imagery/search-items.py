189c580ac84bdebb53ad5d9111a039c60a32876d
ivanhigueram <ivanhigueram@gmail.com>
2017-08-27 20:55:49 +0000

    Update planet search metadata
    This commit removes the hardcoded area of interest to use with the
    Planet API and instead reads the file coords.json which is a dictionary
    with coordinates in geojson format. This allows to search in any AOI by
    only changing the coords.json file and yield a .csv with the imagery
    metadata.

--- src/sat_imagery/search-items.py
+++ src/sat_imagery/search-items.py
@@ -11,34 +11,9 @@ import utils
 from utils import db_connect   
 from src.sat_imagery import gbdx_intersection as sat
 
-aoi = {
-  "type": "Polygon",
-  "coordinates": [
-          [
-            [
-              139,
-              -9
-            ],
-            [
-              145,
-              -9
-            ],
-            [
-              145,
-              -12
-            ],
-            [
-              139,
-              -12
-            ],
-            [
-              139,
-              -9
-            ]
-          ]
-        ]
-  }
-
+with open("coords.json") as file:
+    aoi = json.load(file)
+    file.close()
 
 # will pick up api_key via environment variable PL_API_KEY
 # but can be specified using `api_key` named argument
@@ -67,10 +42,11 @@ request = filters.build_search_request(
 
 result = client.quick_search(request)
 
-
+#Create foluim map
 torres_strait = folium.Map(location=[142, -9],
                    tiles='Mapbox Bright', zoom_start=2)
 folium.GeoJson(aoi, name='geojson').add_to(torres_strait)
+
 
 ##################################################################
 ################## PREPARE DATA: JSON TO FLAT DATA ###############
@@ -78,17 +54,18 @@ folium.GeoJson(aoi, name='geojson').add_to(torres_strait)
 imgs = []
 for item in result.items_iter(limit=None):
     imgs.append(item)
-    #folium.GeoJson(item, name='geojson').add_to(torres_strait)
+    folium.GeoJson(item, name='geojson').add_to(torres_strait)
 
+print("Saving data!")
 #Save request 
-with open('/mnt/data/shared/planet/results_planet_torres_strait.txt', 'w') as file:
+with open('/mnt/data/shared/planet/results_planet_south_china.txt', 'w') as file:
     for item in imgs:
         file.write('%s\n' % item)
     file.close()
 
 #Load requests
 results_gbdx_torres_strait = []
-with open('/mnt/data/shared/planet/results_planet_torres_strait.txt', 'r') as json_file:
+with open('/mnt/data/shared/planet/results_planet_south_china.txt', 'r') as json_file:
     for line in json_file:
         results_gbdx_torres_strait.append(eval(line))
 
@@ -110,13 +87,13 @@ print(metadata_df_polygons.shape)
 
 
 
-#torres_strait.save('torres_strait.html')
+torres_strait.save('torres_strait.html')
 
 ###################################################################
 ######################### IMPORT TO CSV ###########################
 ###################################################################
 
-metadata_df_polygons.to_csv("/mnt/data/shared/planet/planet_torres_strait_metadata.csv", index=False)
+metadata_df_polygons.to_csv("/mnt/data/shared/planet/planet_south_china_metadata.csv", index=False)
 
 
 
