759c7f8dae48547ba75c152acdd6d59e4c0d7405
ivanhigueram <ivanhigueram@gmail.com>
2017-09-01 11:27:43 +0000

    Webapp documentation and changes in code
    New documentation for the RShiny Webapp.

--- shiny_app/data/dummy_data.R
+++ shiny_app/data/dummy_data.R
@@ -2,31 +2,6 @@ library(plyr)
 library(dplyr)
 library(magrittr)
 library(RPostgreSQL)
-#########################################################################
-########################## DATA DUMMY CREATION ##########################
-#########################################################################
-set.seed(1234)
-latemail <- function(N, st="2016/05/01", et="2017/06/15"){
-  st <- as.POSIXct(as.Date(st, tz = 'GMT'))
-  et <- as.POSIXct(as.Date(et, tz = 'GMT'))
-  dt <- as.numeric(difftime(et,st,unit="sec"))
-  ev <- sort(runif(N, 0, dt))
-  rt <- st + ev
-}
-
-create_dummy_ais <- function(number_rows)
-df_dummy = data.frame(matrix(runif(10000, min=1, max=100), nrow=number_rows, ncol = 4, byrow = T)) %>%
-  set_colnames(c("days_fishing", "days_tranship", "days_eez", "num_ais_loss")) %>%
-  mutate(., lat = runif(number_rows, min=-90, max=90),
-         lon = runif(number_rows, min=-180, max=180),
-         mmsi = runif(number_rows, min=10000000, max = 999999999),
-         timestamp = latemail(number_rows)
-  ) %>%
-  dplyr::select(., mmsi, timestamp, lon, lat, days_fishing, days_tranship, days_eez, num_ais_loss)
-
-df_dummy <- create_dummy_ais(10000)
-saveRDS(df_dummy, 'dummy_data.rds')
-
 
 #########################################################################
 ############################ BOOTSTRAP POP-UP  ##########################
@@ -58,19 +33,11 @@ helpPopup <- function(title, content,
 ########################## DATA CONNECTION TO  ##########################
 ############################# POSTGRESQL ################################
 #########################################################################
- # create a connection
- # save the password that we can "hide" it as best as we can by collapsing it
-# pw <- {
-#   "new_user_password"
-# }
- 
+
 drv <- dbDriver("PostgreSQL")
  # creates a connection to the postgres database
  # note that "con" will be used later in each connection to the database
 con <- dbConnect(drv, dbname = Sys.getenv('PGDATABASE'),
                  host = Sys.getenv('PGHOST')  , port = 5432,
                  user = Sys.getenv('PGUSER'), password = Sys.getenv('PGPASSWORD'))
-# rm(pw) # removes the password
- 
-# # check for the cartable
-# dbExistsTable(con, "cartable")
+
